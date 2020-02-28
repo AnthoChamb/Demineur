15aabb06696abbbad34fa5c31ae5e3c97fb41483
Anthony Chamberland <anthochamberland19@icloud.com>
2020-02-28 10:57:01 -0500

    Ajout de la bibliotheque de classes IA

---
+++ IA/IA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IA {
+    /// <summary>Classe de l'intelligence artificielle.</summary>
+    public static class IA {
+        /// <summary>Demande à l'intelligence artificielle de trouver le meilleur coups à jouer.</summary>
+        /// <param name="plateau">Représentation en chaine du plateau de jeu</param>
+        /// <param name="largeur">Largeur du plateau de jeu</param>
+        /// <returns>Retourne l'indice du meilleur coups à jouer</returns>
+        public static int JouerTour(string plateau, int largeur) {
+            return -1;
+        }
+    }
+}
